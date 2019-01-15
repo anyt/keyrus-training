9433701849815c9b8f189f30701e07ee3ecfc456
Andrey Yatsenco <ayatsenko@oroinc.com>
2019-01-15 12:21:19 +0100

    Defined service arguments

--- src/AppBundle/Controller/BrandController.php
+++ src/AppBundle/Controller/BrandController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Oro\Bundle\ProductBundle\Entity\Brand;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -14,11 +15,13 @@ class BrandController extends AbstractController
      *     requirements={"count"="\d+"},
      *     defaults={"count"=10})
      */
-    public function indexAction($count = 10)
+    public function indexAction($count = 10, LoggerInterface $logger)
     {
         if ($count < 1 || $count > 50) {
             throw $this->createNotFoundException('You can render from 1 to 50 brands.');
         }
+
+        $logger->error('{count} brands were requested', ['count' => $count]);
 
         $brands = $this->getDoctrine()->getRepository(Brand::class)->findBy([], [], $count);
 
